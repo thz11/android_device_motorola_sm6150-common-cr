3521463437327871ea3cf930b0041166a5683269
Thiago <thigo6617@gmail.com>
2024-11-22 23:35:34 -0300

    Reapply "sm6150-common: Use common libqti-perfd-client and power-libperfmgr"
    This reverts commit 61a0b953587aff4297a57dfea4b0a986fe15b676.

--- common.mk
+++ common.mk
@@ -420,7 +420,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.sm6150-libperfmgr
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client
 
 # QCOM
 PRODUCT_COPY_FILES += \
@@ -504,7 +505,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
 	$(LOCAL_PATH) \
     hardware/google/interfaces \
-    hardware/google/pixel
+    hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/common/libqti-perfd-client
 
 # Telephony
 PRODUCT_PACKAGES += \
